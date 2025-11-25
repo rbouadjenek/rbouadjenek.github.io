0e4cf6bb6bc00b591ab111ce7c49d6708882890b
rbouadjenek <rbouadjenek@gmail.com>
2025-11-25 13:57:32 +0000

    Deploying to gh-pages from @ rbouadjenek/rbouadjenek.github.io@9b10331a9eeb71dd8edad12dcc1612d509244842 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "rbouadjenek/al-folio",
+    "data-repo": "rbouadjenek/rbouadjenek.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
